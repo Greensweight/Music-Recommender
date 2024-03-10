e7845297b8fa1edab4291b469704bc5860e58617
mdigeron <97755080+mdigeron@users.noreply.github.com>
2024-03-10 19:29:56 -0400

    Add files via upload
    Updated read_rating_V3.py with preliminary HW6 code.

--- read_rating_V3.py
+++ read_rating_V3.py
@@ -3,7 +3,7 @@
 import csv
 import numpy
 
-dataDir='{ADD FILEPATH}'
+dataDir='C:/Users/Marc D/OneDrive - stevens.edu/AAI 627/'
 file_name_test='testTrack_hierarchy.txt'
 file_name_train='trainIdx2_matrix.txt'
 output_file1= 'output3.txt'
@@ -14,7 +14,7 @@ fTrain = open(file_name_train, 'r')
 Trainline = fTrain.readline()
 fOut_complete = open(output_file1, 'w')
 fOut_submission = open(output_file2, 'w')
-header_complete = "UserID|TrackID|AlbumRating|ArtistRating|Genre1Rating|Genre2Rating|Genre3Rating|Genre4Rating|Genre5Rating|Genre6Rating|RatingValue\n"
+header_complete = "UserID|TrackID|AlbumRating|ArtistRating|Genre1Rating|Genre2Rating|Genre3Rating|Genre4Rating|Genre5Rating|Genre6Rating|Genre7Rating|NumberRatedGenres|MaxGenreScore|MinGenreScore|SumGenreScores|AverageGenreScore|VarianceGenreScore|RatingValue\n"
 header_submission = ["TrackID", "Predictor"]
 fOut_complete.write(header_complete)
 csv_writer = csv.writer(fOut_submission)
@@ -29,10 +29,12 @@ genre3ID_vec = [0]*6
 genre4ID_vec = [0]*6
 genre5ID_vec = [0]*6
 genre6ID_vec = [0]*6
+genre7ID_vec = [0]*6
 rating_vec = [0]*6
+genre_vec = [0]*7 # 7 genres
 lastUserID = -1
 
-user_rating_inTrain = numpy.zeros(shape=(6,8)) # change shape from (6,3) to (6,8) to account for genres 
+user_rating_inTrain = numpy.zeros(shape=(6,9)) # change shape from (6,3) to (6,9) to account for genres 
 
 for line in fTest:
     arr_test = line.strip().split('|')
@@ -69,10 +71,14 @@ for line in fTest:
         genre6ID = arr_test[9]
     except IndexError:
         genre6ID = 0
-
+    try:
+        genre7ID = arr_test[10]
+    except IndexError:
+        genre7ID = 0
+        
     if userID != lastUserID:
         ii = 0
-        user_rating_inTrain = numpy.zeros(shape=(6,8)) # Reset for new user
+        user_rating_inTrain = numpy.zeros(shape=(6,9)) # Reset for new user
 
     trackID_vec[ii] = trackID
     albumID_vec[ii] = albumID
@@ -83,6 +89,7 @@ for line in fTest:
     genre4ID_vec[ii] = genre4ID
     genre5ID_vec[ii] = genre5ID
     genre6ID_vec[ii] = genre6ID
+    genre7ID_vec[ii] = genre7ID
     rating_vec[ii] = 0
     ii += 1
     lastUserID = userID
@@ -100,27 +107,86 @@ for line in fTest:
             if trainUserID == userID:
                 for nn in range(6):
                     if trainItemID == albumID_vec[nn]:
-                        user_rating_inTrain[nn, 0] = trainRating
+                        #use if else statements to possibly adjust weights based on rating
+                        if int(trainRating) >= 90:
+                            user_rating_inTrain[nn, 0] = trainRating
+                        else:
+                            user_rating_inTrain[nn, 0] = trainRating
+                        #user_rating_inTrain[nn, 0] = trainRating
                     if trainItemID == artistID_vec[nn]:
-                        user_rating_inTrain[nn, 1] = trainRating
+                        if int(trainRating) >= 90:
+                            user_rating_inTrain[nn, 1] = trainRating
+                        else:
+                            user_rating_inTrain[nn, 1] = trainRating
+                        #user_rating_inTrain[nn, 1] = trainRating
                     if trainItemID == genre1ID_vec[nn]:
-                        user_rating_inTrain[nn, 2] = trainRating
+                        if int(trainRating) >= 90:
+                            user_rating_inTrain[nn, 2] = trainRating
+                        else:
+                            user_rating_inTrain[nn, 2] = trainRating
+                        #user_rating_inTrain[nn, 2] = trainRating
                     if trainItemID == genre2ID_vec[nn]:
-                        user_rating_inTrain[nn, 3] = trainRating
+                        if int(trainRating) >= 90:
+                            user_rating_inTrain[nn, 3] = trainRating
+                        else:
+                            user_rating_inTrain[nn, 3] = trainRating
+                        #user_rating_inTrain[nn, 3] = trainRating
                     if trainItemID == genre3ID_vec[nn]:
-                        user_rating_inTrain[nn, 4] = trainRating
+                        if int(trainRating) >= 90:
+                            user_rating_inTrain[nn, 4] = trainRating
+                        else:
+                            user_rating_inTrain[nn, 4] = trainRating
+                        #user_rating_inTrain[nn, 4] = trainRating
                     if trainItemID == genre4ID_vec[nn]:
-                        user_rating_inTrain[nn, 5] = trainRating
+                        #user_rating_inTrain[nn, 5] = trainRating
+                        if int(trainRating) >= 90:
+                            user_rating_inTrain[nn, 5] = trainRating
+                        else:
+                            user_rating_inTrain[nn, 5] = trainRating
                     if trainItemID == genre5ID_vec[nn]:
-                        user_rating_inTrain[nn, 6] = trainRating
+                        #user_rating_inTrain[nn, 6] = trainRating
+                        if int(trainRating) >= 90:
+                            user_rating_inTrain[nn, 6] = trainRating
+                        else:
+                            user_rating_inTrain[nn, 6] = trainRating
                     if trainItemID == genre6ID_vec[nn]:
-                        user_rating_inTrain[nn, 7] = trainRating
+                        #user_rating_inTrain[nn, 7] = trainRating
+                        if int(trainRating) >= 90:
+                            user_rating_inTrain[nn, 7] = trainRating
+                        else:
+                            user_rating_inTrain[nn, 7] = trainRating
+                    if trainItemID == genre7ID_vec[nn]:
+                        #user_rating_inTrain[nn, 8] = trainRating
+                        if int(trainRating) >= 90:
+                            user_rating_inTrain[nn, 8] = trainRating
+                        else:
+                            user_rating_inTrain[nn, 8] = trainRating
             if trainUserID > userID:
-                
+                number_rated_genres = 0
+                max_genre_score = 0
+                min_genre_score = 0
+                sum_genre = 0
+                average_genre = 0
+                variance_genre = 0
                 for nn in range(6):
+                    for i in range(2,9):
+                        genre_vec[i-2] = user_rating_inTrain[nn, i]
+                        if user_rating_inTrain[nn,i] != 0:
+                            number_rated_genres += 1
+                    max_genre_score = max(genre_vec)
+                    min_genre_score = min(genre_vec)
+                    sum_genre = sum(genre_vec)
+                    if number_rated_genres == 0:
+                        average_genre = 0
+                    else:
+                        average_genre = sum_genre/number_rated_genres
+                    variance_genre = numpy.var(genre_vec)
+                    # copy below line with weights to rating_vec to make it easier to see all of the code
+                    # + number_rated_genres + max_genre_score + min_genre_score + sum_genre + average_genre + variance_genre
                     # rating_vec[nn] stores the scores that we base the rating on modify the below line with weights or other features to improve score
-                    rating_vec[nn] = user_rating_inTrain[nn,0] + user_rating_inTrain[nn,1] + user_rating_inTrain[nn,2] + user_rating_inTrain[nn,3] + user_rating_inTrain[nn,4] + user_rating_inTrain[nn,5] + user_rating_inTrain[nn,6] + user_rating_inTrain[nn,7]
-                    outStr=str(userID) + '|' + str(trackID_vec[nn])+ '|' + str(user_rating_inTrain[nn,0]) + '|' + str(user_rating_inTrain[nn, 1]) + '|' + str(user_rating_inTrain[nn, 2]) + '|' + str(user_rating_inTrain[nn, 3]) + '|' + str(user_rating_inTrain[nn, 4]) + '|' + str(user_rating_inTrain[nn, 5]) + '|' + str(user_rating_inTrain[nn, 6]) + '|' + str(user_rating_inTrain[nn, 7]) + '|' + str(rating_vec[nn])
+                    rating_vec[nn] = user_rating_inTrain[nn,0] + user_rating_inTrain[nn,1] + user_rating_inTrain[nn,2] + user_rating_inTrain[nn,3] + user_rating_inTrain[nn,4] + user_rating_inTrain[nn,5] + user_rating_inTrain[nn,6] + user_rating_inTrain[nn,7] + user_rating_inTrain[nn,8] + number_rated_genres + max_genre_score + min_genre_score + sum_genre + average_genre + variance_genre
+                    outStr=str(userID) + '|' + str(trackID_vec[nn])+ '|' + str(user_rating_inTrain[nn,0]) + '|' + str(user_rating_inTrain[nn, 1]) + '|' + str(user_rating_inTrain[nn, 2]) + '|' + str(user_rating_inTrain[nn, 3]) + '|' + str(user_rating_inTrain[nn, 4]) + '|' + str(user_rating_inTrain[nn, 5]) + '|' + str(user_rating_inTrain[nn, 6]) + '|' + str(user_rating_inTrain[nn, 7]) + '|' + str(user_rating_inTrain[nn, 8]) + '|' + str(number_rated_genres) + '|' + str(max_genre_score) + '|' + str(min_genre_score) + '|' + str(sum_genre) + '|' + str(average_genre) + '|' + str(variance_genre)
+                    outStr = outStr + '|' + str(rating_vec[nn]) # to make sure rating is at end of string
                     fOut_complete.write(outStr + '\n')
                     if nn == 5:
                         rating_vec_sorted = list(reversed(sorted(rating_vec)))
@@ -132,6 +198,7 @@ for line in fTest:
                                 reccomend_count += 1
                             else:
                                 rating_vec[i] = 0
+                        
                 for nn in range(6):
                     csv_writer.writerow([f"{userID}_{trackID_vec[nn]}", int(rating_vec[nn])])
                 break
