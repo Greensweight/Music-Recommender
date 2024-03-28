5a17482fd8e287d2645967cc54b6b84b3ff5830b
mdigeron <97755080+mdigeron@users.noreply.github.com>
2024-03-27 20:04:31 -0400

    Add files via upload

--- matrix_factorization.py
+++ matrix_factorization.py
@@ -1,21 +1,50 @@
 import numpy as np
 import math
-
+from sklearn.decomposition import NMF
+import warnings
+warnings.filterwarnings("error")
 def matrix_factorization(R,P,Q,K, steps, alpha, beta):
-    for step in range(100):
+    for step in range(steps):
+        #print("step: " + str(step))
         for i in range(len(R)):
             for j in range(len(R[i])):
                 if R[i][j] > 0:
                     pq = 0
                     pq2 = 0
                     for k in range(K):
-                        pq += P[i][k]*Q[k][j]
-                        pq2 += P[i][k] * P[i][k] + Q[k][j]*Q[k][j]
-                    e = math.sqrt((R[i][j] - pq)*(R[i][j] - pq) + (beta/2)*pq2)
-                    print(e)
+                        try:
+                            #pq += np.dot(P[i,:],Q[:,j])
+                            pq += P[i][k]*Q[k][j]
+                            #print("pq " + str(pq))
+                            pq2 += P[i][k] * P[i][k] + Q[k][j]*Q[k][j]
+                            #print("pq2 " + str(pq2))
+                        except RuntimeWarning:
+                            print(pq)
+                            print(pq2)
+                            #break
+                            return P, Q 
+                    try:
+                        e = (R[i][j] - pq)
+                        #e = math.sqrt(pow(R[i][j] - np.dot(P[i,:],Q[:,j]), 2) + (beta/2)*pq2)
+                        #print("(R[i][j] - pq): " + str(R[i][j] - pq))
+                        #print("(R[i][j] - pq)*(R[i][j] - pq): " + str((R[i][j] - pq)*(R[i][j] - pq)))
+                        #e = R[i][j] - pq
+                        #print("e " + str(e))
+                    except RuntimeWarning:
+                        print(e)
+                        #break
+                        return P, Q 
                     for k in range(K):
-                        P[i][k] = P[i][k] + alpha * (2*e*Q[k][j]- beta*P[i][k])
-                        Q[k][j] = Q[k][j] + alpha * (2*e*P[i][k]- beta*Q[k][j])
+                        try:
+                            P[i][k] = P[i][k] + alpha * (2*e*Q[k][j]- beta*P[i][k])
+                            #print("Pik " + str(P[i][k]))
+                            Q[k][j] = Q[k][j] + alpha * (2*e*P[i][k]- beta*Q[k][j])
+                            #print("Qkj " + str(Q[k][j]))
+                        except RuntimeWarning:
+                            print(P[i][k])
+                            print(Q[k][j])
+                            #break
+                            return P, Q 
     return P, Q 
 
 
@@ -31,5 +60,13 @@ beta = 0.02
 nP = matrix_factorization(R,P,Q,K, steps, alpha, beta)[0]
 nQ = matrix_factorization(R,P,Q,K, steps, alpha, beta)[1]
 nR = np.matmul(nP,nQ)
+print("Using matrix_factorization function")
 print(R)
 print(nR)
+print("Using sklearn NMR method")
+model = NMF(n_components = K, init="random", random_state = 0, max_iter=5000)
+W = model.fit_transform(R)
+H = model.components_
+nR2 = np.dot(W,H)
+print(R)
+print(nR2)
