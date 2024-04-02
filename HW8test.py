a26dc150d2a908b0e86674919b1c4b33c93dcb15
mdigeron <97755080+mdigeron@users.noreply.github.com>
2024-04-02 10:59:53 -0400

    Optimized df updating

--- HW8test.py
+++ HW8test.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import csv
 import os
+#https://stackoverflow.com/questions/16683701/in-pandas-how-to-get-the-index-of-a-known-value
+
 # use after AAI627spark.ipynb generates myprediction.csv
 
-os.remove("{ADD PATH FOR CSV FILE TO BE DELETED}")
+os.remove("C:/Users/Marc D/OneDrive - stevens.edu/AAI 627/myprediction1_kaggle.csv")
 df = pd.read_csv("myprediction.csv")
 df_old = pd.read_csv("output3.csv")
 # output3.csv is the output from read_rating_V3.py
@@ -58,25 +60,37 @@ fOut_submission.close()
 
 df_new = pd.read_csv(kaggle_output)
 
+"""
 for i in range(len(df_new)):
     if df_new["TrackID"][i] in df_old.values:
         index_old = df_old.loc[df_old["TrackID"] == df_new["TrackID"][i]].index[0]
         index_new = df_new.loc[df_new["TrackID"] == df_new["TrackID"][i]].index[0]
         #df_old.at[index_old, "TrackID"] = df_new.at[index_new, "TrackID"]
         df_old.at[index_old, "Predictor"] = df_new.at[index_new, "Predictor"]
-
+"""
+"""
+for i in range(len(df_new)):
+    if df_new["TrackID"][i] in df_old.values:
+        df_old = df_old.merge(df_new[['TrackID', 'Predictor']], on='TrackID', how='left')
 #df_old is now updated matrix
+"""
+
 """
 print("len df_new: " + str(len(df_new)))
 print("after remapping to updated")
 df_new = df_old
 print("len df_new: " + str(len(df_new)))
 """
+
+df_update= pd.concat([df_old, df_new]).drop_duplicates(subset='TrackID', keep='last').reset_index(drop=True)
+esh_df = df_old[df_old.duplicated("TrackID")]
+df_update= pd.concat([df_update, esh_df]).reset_index(drop=True)
+
 kaggle_output = 'myprediction1_kaggle.csv'
 fOut_submission = open(kaggle_output, 'w')
 csv_writer = csv.writer(fOut_submission)
 header_submission = ["TrackID", "Predictor"]
 csv_writer.writerow(header_submission)
 for i in range(len(df_old)):
-    csv_writer.writerow([f"{df_old['TrackID'][i]}", int(df_old["Predictor"][i])])
+    csv_writer.writerow([f"{df_update['TrackID'][i]}", int(df_update["Predictor"][i])])
 fOut_submission.close()
